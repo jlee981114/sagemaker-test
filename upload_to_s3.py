64ea04c7d9a0bfdafe0f47282824fd9c3a6f13e4
Justin Lee <justinrhee1114@gmail.com>
2024-08-05 12:03:08 -0700

    using upload_to_s3 python script v2

--- upload_to_s3.py
+++ upload_to_s3.py
@@ -1,18 +1,22 @@
 import boto3
 import os
+import glob
 
-def upload_notebook():
+def upload_notebooks():
     s3_bucket = os.environ.get('S3_BUCKET')  # Get S3 bucket name from environment variables
-    s3_key = os.environ.get('S3_KEY')  # Get S3 key from environment variables
+    s3_key_prefix = os.environ.get('S3_KEY_PREFIX')  # Get S3 key prefix from environment variables
 
-    if not s3_bucket or not s3_key:
-        raise ValueError("S3_BUCKET and S3_KEY environment variables must be set")
+    if not s3_bucket or not s3_key_prefix:
+        raise ValueError("S3_BUCKET and S3_KEY_PREFIX environment variables must be set")
 
-    # Upload the notebook to S3
+    # Create S3 client
     s3_client = boto3.client('s3', region_name=os.environ.get('AWS_REGION'))
-    s3_client.upload_file('notebook.ipynb', s3_bucket, s3_key)
 
-    print(f'Notebook uploaded to s3://{s3_bucket}/{s3_key}')
+    # Find all .ipynb files in the current directory
+    for notebook in glob.glob('*.ipynb'):
+        s3_key = f'{s3_key_prefix}/{notebook}'
+        s3_client.upload_file(notebook, s3_bucket, s3_key)
+        print(f'Notebook uploaded to s3://{s3_bucket}/{s3_key}')
 
 if __name__ == "__main__":
-    upload_notebook()
+    upload_notebooks()
